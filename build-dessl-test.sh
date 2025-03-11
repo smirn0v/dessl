1c1f57016feb6be9de4bc06d7bc89c893a7cf2a6
Alexander Smirnov <alexander@smirn0v.ru>
2025-03-11 11:43:40 +0300

    moved main.c to fix go build

--- build-dessl-test.sh
+++ build-dessl-test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-clang -I`pwd`/build -L`pwd`/build -o build/dessl main.c -framework CoreFoundation -framework Security -ldessl
+clang -I`pwd`/build -L`pwd`/build -o build/dessl ./c_example/main.c -framework CoreFoundation -framework Security -ldessl
